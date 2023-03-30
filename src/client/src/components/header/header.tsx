46a40c8dafda02184400cb790088ce6eb46294d4
Andrefox51 <andrea.fornaseri@gmail.com>
2023-03-30 21:13:16 +0200

    Update header.tsx
    esempio

--- src/client/src/components/header/header.tsx
+++ src/client/src/components/header/header.tsx
@@ -9,7 +9,7 @@ export class Header extends React.Component {
         <div className="header-content">
             <h1>dynaware</h1>
             <h2>NAO Challenge 2023</h2>
-            <p>Squadra di Robotica dell'IIS Alessandro Volta di Pescara.</p>
+            <p> di Robotica dell'IIS Alessandro Volta di Pescara.</p>
         </div>
         <img src="https://i.postimg.cc/qRwr1FJz/a5cb68e3-c67f-4d9f-a1ab-1a93d90e6a5a.png"></img>
       </header>
